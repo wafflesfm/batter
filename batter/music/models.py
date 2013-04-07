2749a19e7a437417f1b4db8bf640bb2c0d5c1c54
Edgewyn <edgewyn@waffles.fm>
2013-04-06 19:00:14 -0700

    Artist, ArtistCredit, Release, and ReleaseGroup, plus interactions/migrations.

--- batter/music/models.py
+++ batter/music/models.py
@@ -9,27 +9,25 @@ class Artist(TimeStampedModel):
     begin_date = models.DateField()
     end_date = models.DateField(null=True, blank=True)
     type = models.ForeignKey('ArtistType')
-    country = models.ForeignKey('ArtistCountry')
+    country = models.ForeignKey('Country')
     gender = models.ForeignKey('ArtistGender')
     disambiguation = models.TextField()
 
     def __str__(self):
         return str(self.name)
 
-class ArtistCountry(models.Model):
-    name = models.TextField()
-    code = models.TextField()
+class ArtistCredit(models.Model):
+    name = models.ForeignKey('ArtistName')
+    artists = models.ManyToManyField('Artist')
 
     def __str__(self):
-        return "{} ({})".format(self.name, self.code)
-
+        return str(self.name)
 
 class ArtistGender(models.Model):
     name = models.TextField()
 
     def __str__(self):
         return self.name
-
 
 class ArtistName(models.Model):
     name = models.TextField()
@@ -43,3 +41,49 @@ class ArtistType(models.Model):
     def __str__(self):
         return self.name
 
+class Country(models.Model):
+    name = models.TextField()
+    code = models.TextField()
+
+    def __str__(self):
+        return "{} ({})".format(self.name, self.code)
+
+class Release(TimeStampedModel):
+    mbid = models.TextField()
+    name = models.ForeignKey('ReleaseName')
+    artist_credit = models.ForeignKey('ArtistCredit')
+    group = models.ForeignKey('ReleaseGroup')
+#    status = models.ForeignKey('ReleaseStatus')
+#    packaging = models.ForeignKey('ReleasePackaging')
+    country = models.ForeignKey('Country')
+    date = models.DateField()
+    barcode = models.TextField()
+    comment = models.TextField()
+
+    def __str__(self):
+        return str(self.name)
+
+class ReleaseName(models.Model):
+    name = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+#class ReleasePackaging(models.Model):
+#    name = models.TextField()
+
+#class ReleaseStatus(models.Model):
+#    name = models.TextField()
+
+class ReleaseGroup(TimeStampedModel):
+    mbid = models.TextField()
+    name = models.ForeignKey('ReleaseName')
+    credit = models.ForeignKey('ArtistCredit')
+#    type = models.ForeignKey('ReleaseGroupPrimaryType')
+    comment = models.TextField()
+
+    def __str__(self):
+        return str(self.name)
+
+#class ReleaseGroupPrimaryType(models.Model):
+#    name = models.TextField()
