bfa9ee672cc6dea59a3c095210b91d208a9bf697
Edgewyn <edgewyn@waffles.fm>
2013-04-10 15:30:44 -0400

    Disallow no file / empty files on upload

--- batter/torrents/fields.py
+++ batter/torrents/fields.py
@@ -6,8 +6,11 @@ from django.core.exceptions import ValidationError
 from .models import Torrent
 
 class TorrentField(forms.FileField):
-    def to_python(self, value):
+    def to_python(self, data):
+        data = super(TorrentField, self).to_python(data)
+        if data is None:
+            raise ValidationError(self.error_messages['empty'])
         try:
-            return Torrent.from_torrent_file(value)
+            return Torrent.from_torrent_file(data)
         except BTFailure as e:
             raise ValidationError(str(e))
