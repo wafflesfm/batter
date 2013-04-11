67ec091d9b2434cd180ccfe14ed9c4229c2b4b74
Ell <alan@dynamitealley.com>
2013-04-11 23:37:01 +0000

    cleaning up some more

--- batter/music/helpers/discogs.py
+++ batter/music/helpers/discogs.py
@@ -20,29 +20,35 @@ class DiscogsAPI(object):
         request = requests.get(url, params=params, headers=headers)
         return request.json()
 
-    def search(self, type, query, page=1):
-        params = {
-            'q': query
-        }
-        return self.make_request('/database/search', **params)
+    def search_request(self, query, search_type=None, page=1):
+        page += 1
+        params = {'q': query}
+
+        if search_type:
+            params['type'] = search_type
+
+        search = self.make_request('/database/search', **params)
+        yield search['results']
+
+        while page < search['pagination']['pages']:
+            params['page'] = page
+            yield self.make_request('/database/search', **params)['results']
+            page += 1
+
+    def search(self, query, page=1):
+        return self.search_request(query, page=page)
 
     def search_artist(self, query, page=1):
-        params = {
-            'q': query,
-            'type': 'artist',
-        }
-        return self.make_request('/database/search', **params)
+        return self.search_request(query, search_type='artist', page=page)
 
     def search_release(self, query, page=1):
-        params = {
-            'q': query,
-            'type': 'release',
-        }
-        return self.make_request('/database/search', **params)
+        return self.search_request(query, search_type='release', page=page)
 
     def get_artist(self, artist_id):
-        artist = self.make_request('/artists/%s' % artist_id, page=page)
-        return artist
+        return self.make_request('/artists/%s' % artist_id, page=page)
+
+    def get_release(self, release_id):
+        return self.make_request('/releases/%s' % release_id)
 
     def get_releases(self, artist_id, page=1):
         """
@@ -50,7 +56,6 @@ class DiscogsAPI(object):
         ``get_releases(45).next()`` returns a generator for all pages of releases
         takes options page argument for starting page
         """
-
         page += 1
         releases = self.make_request('/artists/%s/releases' % artist_id)
         yield releases['releases']
@@ -60,17 +65,8 @@ class DiscogsAPI(object):
             yield self.make_request('/artists/%s/releases' % artist_id, **params)['releases']
             page += 1
 
-    def get_release(self, release_id):
-        release = self.make_request('/releases/%s' % release_id)
-        return release
-
 
 if __name__ == '__main__':
     d = DiscogsAPI()
-    releases = d.get_releases(45) # Aphex Twin
-    print releases.next()[0]['id']
-    print releases.next()[0]['id']
-    print releases.next()[0]['id']
-    print releases.next()[0]['id']
-    print releases.next()[0]['id']
-    print releases.next()[0]['id'] 
+    search = d.search_release('window licker')
+    print search.next()[0]['id']
