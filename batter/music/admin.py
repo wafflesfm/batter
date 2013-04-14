e13bf8738c18fec8cdd43766c224854268b48a53
Ell <alan@dynamitealley.com>
2013-04-14 11:25:14 -0700

    god damn blank lines

--- batter/music/admin.py
+++ batter/music/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from music.models import MusicUpload, Artist, ArtistType, ArtistAlias, \
     Country, Release
 
-                
+
 admin.site.register(Artist)
 admin.site.register(Release)
 admin.site.register(MusicUpload)
