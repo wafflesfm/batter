f337a93afae58b4a457c8fd018a570f55ccb7890
edgewyn <edgewyn@waffles.fm>
2013-07-28 20:57:17 -0700

    Serve media files in development.

--- batter/batter/urls.py
+++ batter/batter/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
@@ -20,3 +22,7 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^grappelli/', include('grappelli.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
