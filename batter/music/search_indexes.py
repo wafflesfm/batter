72384d6a79999051ae8a6547f6e638134a6d699d
edgewyn <edgewyn@waffles.fm>
2013-08-07 23:40:45 -0700

    Add Master search index and document search template.

--- batter/music/search_indexes.py
+++ batter/music/search_indexes.py
@@ -1,6 +1,6 @@
 from haystack import indexes
 
-from .models import Artist
+from .models import Artist, Master
 
 
 class ArtistIndex(indexes.SearchIndex, indexes.Indexable):
@@ -8,6 +8,16 @@ class ArtistIndex(indexes.SearchIndex, indexes.Indexable):
 
     def get_model(self):
         return Artist
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
+
+
+class MasterIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.EdgeNgramField(document=True, use_template=True)
+
+    def get_model(self):
+        return Master
 
     def index_queryset(self, using=None):
         return self.get_model().objects.all()
