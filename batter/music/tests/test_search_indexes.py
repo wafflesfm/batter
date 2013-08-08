bc4473f34842b5c069d1196848680fbc6bbf7133
edgewyn <edgewyn@waffles.fm>
2013-08-07 23:53:26 -0700

    Tests for Master search indexes.

--- batter/music/tests/test_search_indexes.py
+++ batter/music/tests/test_search_indexes.py
@@ -2,8 +2,8 @@ from __future__ import absolute_import, unicode_literals
 
 from django.test import TestCase
 
-from ..models import Artist
-from ..search_indexes import ArtistIndex
+from ..models import Artist, Master
+from ..search_indexes import ArtistIndex, MasterIndex
 
 
 class ArtistIndexTests(TestCase):
@@ -12,6 +12,20 @@ class ArtistIndexTests(TestCase):
 
     def test_get_model(self):
         self.assertEquals(self.index.get_model(), Artist)
+
+    def test_index_queryset(self):
+        # Querysets are covered by Django tests, so just make sure that the QS
+        # model is the same as the index model.
+        self.assertEquals(self.index.index_queryset().model,
+                          self.index.get_model())
+
+
+class MasterIndexTests(TestCase):
+    def setUp(self):
+        self.index = MasterIndex()
+
+    def test_get_model(self):
+        self.assertEquals(self.index.get_model(), Master)
 
     def test_index_queryset(self):
         # Querysets are covered by Django tests, so just make sure that the QS
